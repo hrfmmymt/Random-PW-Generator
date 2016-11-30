5d78cc236f052e0f87fd883f6d117aa1274050c4
hrfmmymt <hrfmmymt@gmail.com>
2016-11-30 18:28:54 +0900

    add validation.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
+"use strict";
+
+const passLen = document.getElementById("passlen");
+const lowerLetter = document.getElementById("lower");
+const upperLetter = document.getElementById("upper");
+const numLetter = document.getElementById("numbers");
+const symLetter = document.getElementById("symbols");
+const pasText = document.getElementById("pasText");
+
 const generatePassword = () => {
-  const lunPass = document.getElementById("passlen").value;
-  const lowerLetter = document.getElementById("lower").checked;
-  const upperLetter = document.getElementById("upper").checked;
-  const numLetter = document.getElementById("numbers").checked;
-  const symLetter = document.getElementById("symbols").checked;
+  const lunPass = Number(passLen.value);
 
   const el = [];
   let numberOfCharacters = 0;
 
-  if (lowerLetter) {
+  if (lowerLetter.checked) {
     el.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
     numberOfCharacters = 26;
   }
-  if (upperLetter) {
+  if (upperLetter.checked) {
     el.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
     numberOfCharacters += 26;
   }
-  if (numLetter) {
+  if (numLetter.checked) {
     el.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
     numberOfCharacters += 10;
   }
-  if (symLetter) {
+  if (symLetter.checked) {
     el.push("!", "\"", "£", "$", "%", "&", "/", "(", ")", "=", "?", "^", "'", "*", "+", "-", "_", ".", ":", ",", ";", ".", ":");
     numberOfCharacters += 23;
   }
@@ -29,10 +34,9 @@ const generatePassword = () => {
 
   for (let i = 0; i < lunPass; i++) {
     const randomNumber = Math.floor(Math.random() * numberOfCharacters);
-    password += el[randomNumber];
+    password += el[randomNumber] || "";
   }
 
-  const pasText = document.getElementById("pasText");
   pasText.value = password;
   pasText.className = "show";
 
@@ -46,5 +50,21 @@ const generatePassword = () => {
   }, 1000);
 };
 
-const el = document.getElementById("genepass");
-el.addEventListener("click", generatePassword, false);
+const genPassBtn = document.getElementById("genePass");
+genPassBtn.addEventListener("click", generatePassword);
+
+const disabledButton = () => {
+  if (!lowerLetter.checked && !upperLetter.checked && !numLetter.checked && !symLetter.checked) {
+    genPassBtn.setAttribute("disabled", "");
+  } else if (passLen.value === "") {
+    genPassBtn.setAttribute("disabled", "");
+  } else {
+    genPassBtn.removeAttribute("disabled");
+  }
+};
+
+const checkBox = document.querySelectorAll("input[type='checkbox'], input[type='number']");
+
+Array.from(checkBox).forEach(list => {
+  list.addEventListener("change", disabledButton);
+});
