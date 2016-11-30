82e3db06cd60d7b1d313c68c487d6f15eeddfd6d
hrfmmymt <hrfmmymt@gmail.com>
2016-11-30 13:49:26 +0900

    use xo.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,50 @@
-function generatePassword() {
+var generatePassword = () => {
+  const lunPass = document.getElementById("passlen").value;
+  const lowerLetter = document.getElementById("lower").checked;
+  const upperLetter = document.getElementById("upper").checked;
+  const numLetter = document.getElementById("numbers").checked;
+  const symLetter = document.getElementById("symbols").checked;
 
-  var lunPass = document.querySelector('#passlen').value;
-  var lowerLetter = document.querySelector('#lower').checked;
-  var upperLetter = document.querySelector('#upper').checked;
-  var numLetter = document.querySelector('#numbers').checked;
-  var symLetter = document.querySelector('#symbols').checked;
+  const el = [];
+  let numberOfCharacters = 0;
 
-  var el = [];
-  var numberOfCharacters = 0;
-
-  if(lowerLetter) {
-    el.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+  if (lowerLetter) {
+    el.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
     numberOfCharacters = 26;
   }
-  if(upperLetter) {
-    el.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-    numberOfCharacters = numberOfCharacters + 26;
+  if (upperLetter) {
+    el.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    numberOfCharacters += 26;
   }
-  if(numLetter) {
-    el.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-    numberOfCharacters = numberOfCharacters + 10;
+  if (numLetter) {
+    el.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    numberOfCharacters += 10;
   }
-  if(symLetter) {
-    el.push('!', '\"', '£', '$', '%', '&', '/', '(', ')', '=', '?', '^', '\'', '*', '+', '-', '_', '.', ':', ',', ';', '.', ':');
-    numberOfCharacters = numberOfCharacters + 23;
+  if (symLetter) {
+    el.push("!", "\"", "£", "$", "%", "&", "/", "(", ")", "=", "?", "^", "'", "*", "+", "-", "_", ".", ":", ",", ";", ".", ":");
+    numberOfCharacters += 23;
   }
 
-  var password = '';
+  let password = "";
 
-  for(var i = 0; i < lunPass; i++) {
-    var randomNumber = Math.floor(Math.random() * numberOfCharacters);
+  for (let i = 0; i < lunPass; i++) {
+    const randomNumber = Math.floor(Math.random() * numberOfCharacters);
     password += el[randomNumber];
   }
 
-  var pasText = document.querySelector('#pasText');
+  const pasText = document.getElementById("pasText");
   pasText.value = password;
-  pasText.className = 'show';
+  pasText.className = "show";
 
   pasText.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
 
-  var copied = document.querySelector('#copied');
-  copied.style.display = 'block';
-  setTimeout(function() {
-    copied.style.display = 'none';
-  }, 2000);
+  const copied = document.getElementById("copied");
+  copied.style.display = "block";
+  setTimeout(() => {
+    copied.style.display = "none";
+  }, 1000);
+};
 
-}
-
-var el = document.querySelector('#genepass');
-el.addEventListener('click', generatePassword, false);
+const el = document.getElementById("genepass");
+el.addEventListener("click", generatePassword, false);
--- main.js
+++ main.js
@@ -1,132 +1,133 @@
-'use strict';
+"use strict";
 
-const {app, BrowserWindow, Menu} = require('electron');
+const {app, BrowserWindow, Menu} = require("electron");
+const path = require("path");
 
 let mainWindow;
 
-app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    width: 480,
-    height: 640,
-  });
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  installMenu();
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-});
-
 const installMenu = () => {
-  if(process.platform === 'darwin') {
+  if (process.platform === "darwin") {
     const menu = Menu.buildFromTemplate([
       {
-        label: 'PW-copy-Generator',
+        label: "PW-copy-Generator",
         submenu: [
           {
-            label: 'About PW-copy-Generator',
-            selector: 'orderFrontStandardAboutPanel:'
+            label: "About PW-copy-Generator",
+            selector: "orderFrontStandardAboutPanel:"
           },
           {
-            type: 'separator'
+            type: "separator"
           },
           {
-            label: 'Services',
+            label: "Services",
             submenu: []
           },
           {
-            type: 'separator'
+            type: "separator"
           },
           {
-            label: 'Hide PW-copy-Generator',
-            accelerator: 'Command+H',
-            selector: 'hide:'
+            label: "Hide PW-copy-Generator",
+            accelerator: "Command+H",
+            selector: "hide:"
           },
           {
-            label: 'Hide Others',
-            accelerator: 'Command+Shift+H',
-            selector: 'hideOtherApplications:'
+            label: "Hide Others",
+            accelerator: "Command+Shift+H",
+            selector: "hideOtherApplications:"
           },
           {
-            label: 'Show All',
-            selector: 'unhideAllApplications:'
+            label: "Show All",
+            selector: "unhideAllApplications:"
           },
           {
-            type: 'separator'
+            type: "separator"
           },
           {
-            label: 'Quit',
-            accelerator: 'Command+Q',
+            label: "Quit",
+            accelerator: "Command+Q",
             click() {
               app.quit();
             }
-          },
+          }
         ]
       },
       {
-        label: 'View',
+        label: "View",
         submenu: [
           {
-            label: 'Reload',
-            accelerator: 'Command+R',
+            label: "Reload",
+            accelerator: "Command+R",
             click() {
               mainWindow.reload();
             }
           },
           {
-            label: 'Toggle Full Screen',
-            accelerator: 'Ctrl+Command+F',
+            label: "Toggle Full Screen",
+            accelerator: "Ctrl+Command+F",
             click() {
               mainWindow.setFullScreen(!mainWindow.isFullScreen());
             }
           },
           {
-            label: 'Toggle Developer Tools',
-            accelerator: 'Alt+Command+I',
+            label: "Toggle Developer Tools",
+            accelerator: "Alt+Command+I",
             click() {
               mainWindow.toggleDevTools();
             }
-          },
+          }
         ]
       }
     ]);
     Menu.setApplicationMenu(menu);
   } else {
-    menu = Menu.buildFromTemplate([
+    const menu = Menu.buildFromTemplate([
       {
-        label: '&View',
+        label: "&View",
         submenu: [
           {
-            label: '&Reload',
-            accelerator: 'Ctrl+R',
+            label: "&Reload",
+            accelerator: "Ctrl+R",
             click() {
               mainWindow.reload();
             }
           },
           {
-            label: 'Toggle &Full Screen',
-            accelerator: 'F11',
+            label: "Toggle &Full Screen",
+            accelerator: "F11",
             click() {
               mainWindow.setFullScreen(!mainWindow.isFullScreen());
             }
           },
           {
-            label: 'Toggle &Developer Tools',
-            accelerator: 'Alt+Ctrl+I',
+            label: "Toggle &Developer Tools",
+            accelerator: "Alt+Ctrl+I",
             click() {
               mainWindow.toggleDevTools();
             }
-          },
+          }
         ]
       }
     ]);
     mainWindow.setMenu(menu);
   }
+};
 
-}
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 480,
+    height: 640
+  });
+  const url = path.join(__dirname, "/index.html");
+  mainWindow.loadURL("file://" + url);
+  installMenu();
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
